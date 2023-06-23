84f024ef5edee70f8ccfc2cf5bbcdf7f3cc99157
aravind <uaravindshenoy@gmail.com>
2023-06-23 19:44:38 +0530

    Tracking changes in title, amount and date inputs using useState and onChange attribute

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,35 @@
+import { useState } from 'react'
+
 import './ExpenseForm.css'
 
 function ExpenseForm(){
-
+    const [enteredTitle,setEnteredTitle] = useState('');
+    const [enteredAmount,setEnteredAmount] = useState(0);
+    const [enteredDate,setEnteredDate] = useState('');
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value)
+        console.log(event.target.value);
+    }
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+    }
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+    }
     return (
         <form>
             <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label htmlFor="title" >Enter title here.</label>
-                <input id="title" type="text" />
+                <input onChange={titleChangeHandler} id="title" type="text" />
                 </div>
                 <div className='new-expense__control'>
                 <label htmlFor="amount" >Enter amount here.</label>
-                <input id="amount" type="number" min="0.01" step="0.01" />
+                <input onChange={amountChangeHandler} id="amount" type="number" min="0.01" step="0.01" />
                 </div>
                 <div className='new-expense__control'>
                 <label htmlFor="date" >Enter date here.</label>
-                <input id="date" type="date" min="2019-01-01" max="2022-12-31" />
+                <input onChange={dateChangeHandler} id="date" type="date" min="2019-01-01" max="2022-12-31" />
                 </div>
                 <button>Add Expense</button>
                 </div>
